f49603f9d12f55366c1a80fc18f0712eef4170fe
LeightonGuang <leighton.guang@icloud.com>
2024-11-06 06:35:24 +0000

    Feat: New blinds page

---
+++ app/blinds/page.tsx
@@ -0,0 +1,114 @@
+"use client";
+
+import Image from "next/image";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+  TableRow,
+} from "@nextui-org/table";
+import { Tooltip } from "@nextui-org/react";
+
+import blindsData from "@/public/data/blindData.json";
+import { title } from "@/components/primitives";
+
+export default function SmokePage() {
+  const flashColumns = [
+    { name: "agent", sortable: true },
+    { name: "ability", sortable: false },
+    { name: "blind_duration", sortable: true },
+    { name: "charge", sortable: true },
+    { name: "cost", sortable: true },
+    { name: "regen", sortable: true },
+    { name: "health", sortable: true },
+  ];
+  const nearsigntColumns = [
+    { name: "agent", sortable: true },
+    { name: "ability", sortable: false },
+    { name: "blind_duration", sortable: true },
+    { name: "charge", sortable: true },
+    { name: "cost", sortable: true },
+    { name: "regen", sortable: true },
+    { name: "health", sortable: true },
+  ];
+
+  return (
+    <section>
+      <h1 className={title()}>Blinds</h1>
+      <div>
+        <h2 className="mt-6">Flash</h2>
+        <Table className="mt-4" fullWidth={true}>
+          <TableHeader>
+            {flashColumns.map((column) => (
+              <TableColumn key={column.name}>{column.name}</TableColumn>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {blindsData.flashData.map((flash) => (
+              <TableRow key={flash.id}>
+                <TableCell>{flash.agent}</TableCell>
+                <TableCell>
+                  <Tooltip content={flash.ability.name}>
+                    <div className="cursor-pointer">
+                      <Image
+                        unoptimized
+                        alt={flash.ability.name}
+                        height={32}
+                        src={flash.ability.iconUrl}
+                        width={32}
+                      />
+                    </div>
+                  </Tooltip>
+                </TableCell>
+                <TableCell>{flash.blind_duration}</TableCell>
+                <TableCell>{flash.charge}</TableCell>
+                <TableCell>{flash.cost}</TableCell>
+                <TableCell>
+                  {flash.regen.reusable ? flash.regen.regenTime : "x"}
+                </TableCell>
+                <TableCell>{flash.health ? flash.health : "-"}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+      <div className="mt-6">
+        <h2 className="mt-6">Nearsight</h2>
+        <Table className="mt-4" selectionMode="single">
+          <TableHeader>
+            {nearsigntColumns.map((column) => (
+              <TableColumn key={column.name}>{column.name}</TableColumn>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {blindsData.nearsigntData.map((nearsight) => (
+              <TableRow key={nearsight.id}>
+                <TableCell>{nearsight.agent}</TableCell>
+                <TableCell>
+                  <Image
+                    unoptimized
+                    alt={nearsight.ability.name}
+                    height={32}
+                    src={nearsight.ability.iconUrl}
+                    width={32}
+                  />
+                </TableCell>
+                <TableCell>{nearsight.blind_duration}</TableCell>
+                <TableCell>{nearsight.charge}</TableCell>
+                <TableCell>{nearsight.cost}</TableCell>
+                <TableCell>
+                  {nearsight.regen.reusable ? nearsight.regen.regenTime : "x"}
+                </TableCell>
+                <TableCell>
+                  {nearsight.health ? nearsight.health : "-"}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </section>
+  );
+}
