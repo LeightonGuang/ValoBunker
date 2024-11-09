d4419da2a707264a91e0091f97a1571b0d1afaae
LeightonGuang <leighton.guang@icloud.com>
2024-11-09 02:48:18 +0000

    Feat: New list icon

--- components/icons.tsx
+++ components/icons.tsx
@@ -231,3 +231,21 @@ export const MenuIcon = (props: IconSvgProps) => (
     />
   </svg>
 );
+
+export const ListIcon = (props: IconSvgProps) => (
+  <svg
+    className="size-6"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="1.5"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0ZM3.75 12h.007v.008H3.75V12Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm-.375 5.25h.007v.008H3.75v-.008Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </svg>
+);
