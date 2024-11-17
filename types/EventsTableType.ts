aafd0572e2511ccabd6b375f1038edbf5692ca7f
LeightonGuang <leighton.guang@icloud.com>
2024-11-17 07:16:18 +0000

    Feat: New type for events table

---
+++ types/EventsTableType.ts
@@ -0,0 +1,10 @@
+export interface EventsTableType {
+  id: number;
+  type: string;
+  name: string;
+  event_icon_url: string;
+  location: string;
+  start_date: string;
+  end_date: string;
+  created_at: string;
+}
