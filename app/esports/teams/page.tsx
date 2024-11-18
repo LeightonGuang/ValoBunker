fcfdc9371cbd2cd1b3d96c966598b6983fc0e662
LeightonGuang <leighton.guang@icloud.com>
2024-11-18 07:37:59 +0000

    Feat: Update teams page to use supabase data

--- app/esports/teams/page.tsx
+++ app/esports/teams/page.tsx
@@ -1,25 +1,25 @@
 "use client";
 
 import {
-  Avatar,
-  Button,
-  Card,
-  CardBody,
-  CardFooter,
-  CardHeader,
+  Accordion,
+  AccordionItem,
   Divider,
+  Image,
   Tab,
   Tabs,
+  User,
 } from "@nextui-org/react";
-import Image from "next/image";
-import Link from "next/link";
-
-import teamsData from "../../../public/data/teamsData.json";
+import { useEffect, useMemo, useState } from "react";
 
 import { title } from "@/components/primitives";
-import { ListIcon } from "@/components/icons";
+import { getSupabase } from "@/utils/supabase/client";
+import { TeamsTableType } from "@/types/TeamsTableType";
 
-const regionList: { region: string; region_icon_url: string }[] = [
+const regionTabs: { region: string; region_icon_url: string }[] = [
+  {
+    region: "All",
+    region_icon_url: "https://cdn3.emoji.gg/emojis/7053-vct-red-logo.png",
+  },
   {
     region: "Americas",
     region_icon_url: "https://owcdn.net/img/640f5ab71dfbb.png",
@@ -39,6 +39,43 @@ const regionList: { region: string; region_icon_url: string }[] = [
 ];
 
 const TeamsPage = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [regionTab, setRegionTab] = useState("All");
+  const [teamsDataList, setTeamsDataList] = useState<TeamsTableType[]>([]);
+
+  const getAllTeams = async () => {
+    try {
+      const supabase = getSupabase();
+      const { data, error } = await supabase
+        .from("teams")
+        .select(`*, players(*), coaches(*)`)
+        .order("name", { ascending: true });
+
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(data);
+        setTeamsDataList(data);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const filteredTeamList = useMemo(() => {
+    return [...teamsDataList].filter((team) => {
+      if (regionTab === "All") return true;
+
+      return team.vct_league === regionTab;
+    });
+  }, [regionTab, teamsDataList]);
+
+  useEffect(() => {
+    getAllTeams();
+  }, []);
+
   return (
     <section>
       <div>
@@ -47,114 +84,91 @@ const TeamsPage = () => {
       <div>
         <Tabs
           className="mt-4 flex justify-center"
-          classNames={{
-            tab: "max-w-fit",
+          classNames={{ tabList: "gap-0 lg:gap-2", tab: "max-w-fit h-max" }}
+          onSelectionChange={(key) => {
+            setRegionTab(String(key));
           }}
         >
-          {Object.values(teamsData).map((regionData, i) => (
+          {regionTabs.map((tab, i) => (
             <Tab
-              key={regionList[i].region}
-              className="h-full w-full"
+              key={regionTabs[i].region}
               title={
                 <div className="flex flex-col items-center gap-2 lg:flex-row">
                   <Image
-                    unoptimized
-                    alt={regionList[i].region}
+                    alt={tab.region}
                     className="h-4 w-4"
                     height={16}
-                    src={regionList[i].region_icon_url}
+                    src={tab.region_icon_url}
                     width={16}
                   />
-                  {regionList[i].region}
+                  {tab.region}
                 </div>
               }
             >
-              <Card className="mt-6 w-min lg:hidden">
-                <CardHeader>
-                  Contents <ListIcon className="ml-2 h-4 w-4" />
-                </CardHeader>
-                <CardBody>
-                  <ul className="list-decimal text-sm text-default-500 marker:text-default-500">
-                    {regionData.map((teamObj) => (
-                      <li key={teamObj.id} className="w-max">
-                        <Link
-                          className="hover:underline"
-                          href={`#teamsPage__${regionList[i]}--${teamObj.id}`}
-                        >
-                          {teamObj.team_name}
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </CardBody>
-              </Card>
-              <div className="mt-4 flex flex-col gap-16 py-4 lg:snap-x lg:snap-mandatory lg:flex-row lg:flex-nowrap lg:gap-8 lg:overflow-x-scroll">
-                {regionData.map((teamObj) => (
-                  <Card
+              <Accordion
+                className="mt-4 flex flex-col gap-4"
+                showDivider={false}
+              >
+                {filteredTeamList.map((teamObj) => (
+                  <AccordionItem
                     key={teamObj.id}
-                    className="lg:w-[calc(25%-1.5rem)] lg:flex-shrink-0 lg:snap-start"
-                    id={`teamsPage__${regionList[i]}--${teamObj.id}`}
-                  >
-                    <CardHeader>
+                    aria-label={teamObj.name}
+                    startContent={
                       <Image
-                        unoptimized
-                        alt={teamObj.team_name}
-                        className="[0.875rem] mr-4 rounded-md"
-                        height={64}
-                        src={teamObj.team_logo_url}
-                        width={64}
+                        classNames={{ img: "rounded-none" }}
+                        height={48}
+                        src={teamObj.logo_url}
+                        width={48}
                       />
-                      <h2 className="text-xl font-bold">{teamObj.team_name}</h2>
-                    </CardHeader>
-                    <Divider />
-                    <CardBody>
-                      <h2>Main Roster</h2>
-                      <ul className="mt-4 flex list-disc flex-col gap-4">
-                        {teamObj.roster.map((player, i) => (
-                          <li key={player.id} className="flex">
-                            <Button className="flex h-full w-full p-2">
-                              <Avatar
-                                className="ml-[0.625rem] min-w-[2.5rem] bg-[#ffffff]"
-                                color="primary"
-                                isBordered={i === 0}
-                                size="md"
-                                src={player.profile_picture_url}
+                    }
+                    title={teamObj.name}
+                    variant="splitted"
+                  >
+                    <div>
+                      <h2>Players</h2>
+                      <ul className="mt-4 flex flex-col gap-4">
+                        {[...teamObj.players]
+                          .sort((a, b) =>
+                            a.role === "IGL" ? -1 : b.role === "IGL" ? 1 : 0,
+                          )
+                          .map((player) => (
+                            <li key={player.id}>
+                              <User
+                                avatarProps={{
+                                  src: player.profile_picture_url,
+                                  isBordered: player.role === "IGL",
+                                  color:
+                                    player.role === "IGL"
+                                      ? "primary"
+                                      : "default",
+                                  classNames: {
+                                    base: "bg-white",
+                                  },
+                                }}
+                                description={
+                                  <div className="flex flex-col">
+                                    <span>{player.name}</span>
+                                    <span>{player.role}</span>
+                                  </div>
+                                }
+                                name={player.ign}
                               />
-                              <div className="ml-[1.25rem] flex w-full flex-col text-left">
-                                <p className="text-[1rem]">{player.ign}</p>
-                                <p className="text-[0.8rem] text-default-500">
-                                  {player.name}
-                                </p>
-                                <p className="text-[0.8rem] text-default-500">
-                                  {player.role}
-                                </p>
-                              </div>
-                            </Button>
-                          </li>
-                        ))}
+                            </li>
+                          ))}
                       </ul>
-                    </CardBody>
-                    <Divider />
-                    <CardFooter>
-                      <Button className="flex h-full w-full justify-between p-2">
-                        <Avatar
-                          className="ml-[0.625rem] min-w-[2.5rem] bg-[#ffffff]"
-                          src={teamObj.head_coach.profile_picture_url}
-                        />
-                        <div className="ml-[1.25rem] flex w-full flex-col text-left">
-                          <p>{teamObj.head_coach.ign}</p>
-                          <p className="text-[0.8rem] text-default-500">
-                            {teamObj.head_coach.name}
-                          </p>
-                          <p className="text-[0.8rem] text-default-500">
-                            Head Coach
-                          </p>
-                        </div>
-                      </Button>
-                    </CardFooter>
-                  </Card>
+                      <Divider className="my-4" />
+                      <h2>Coaches</h2>
+                      <div className="mt-4">
+                        {teamObj.coaches.length === 0 ? (
+                          <User name="N/A" />
+                        ) : (
+                          <User name={teamObj.coaches[0].name} />
+                        )}
+                      </div>
+                    </div>
+                  </AccordionItem>
                 ))}
-              </div>
+              </Accordion>
             </Tab>
           ))}
         </Tabs>
