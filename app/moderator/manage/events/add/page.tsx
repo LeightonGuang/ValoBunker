39c668acda8a2dda752dd0c373dec7e981199796
LeightonGuang <leighton.guang@icloud.com>
2024-11-22 20:20:20 +0000

    Feat: New add event page

---
+++ app/moderator/manage/events/add/page.tsx
@@ -0,0 +1,160 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Button, Card, CardBody, Image, Input } from "@nextui-org/react";
+
+import { title } from "@/components/primitives";
+import { getSupabase } from "@/utils/supabase/client";
+
+interface AddEventFormType {
+  event_icon_url: string;
+  type: string;
+  name: string;
+  start_date: string;
+  end_date: string;
+  location: string;
+}
+
+const AddEventPage = () => {
+  const [addEventForm, setAddEventForm] = useState<AddEventFormType>({
+    event_icon_url: "",
+    type: "",
+    name: "",
+    start_date: "",
+    end_date: "",
+    location: "",
+  });
+  const router = useRouter();
+
+  const onAddEventFormChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    setAddEventForm({
+      ...addEventForm,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleAddEventFormSubmit = async () => {
+    try {
+      const supabase = getSupabase();
+
+      const { data, error } = await supabase
+        .from("events")
+        .insert([addEventForm]);
+
+      if (error) {
+        console.error(error);
+      } else {
+        console.log(data);
+        router.push("/moderator/manage/events");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    console.log(addEventForm);
+  }, [addEventForm]);
+
+  return (
+    <section>
+      <h1 className={title()}>Add Event</h1>
+      <div className="mt-6 flex justify-center">
+        <Card className="w-96">
+          <CardBody>
+            <form className="flex flex-col gap-4">
+              <div className="flex justify-center">
+                <Image
+                  className="max-h-40"
+                  src={
+                    addEventForm.event_icon_url === ""
+                      ? "https://placehold.co/1920x1080"
+                      : addEventForm.event_icon_url
+                  }
+                />
+              </div>
+              <label htmlFor="event_icon_url">
+                Event Icon Url
+                <Input
+                  name="event_icon_url"
+                  placeholder="Event Icon Url"
+                  type="text"
+                  onChange={onAddEventFormChange}
+                />
+              </label>
+
+              <label htmlFor="name">
+                Type
+                <Input
+                  name="type"
+                  placeholder="eg, Champions, Masters"
+                  type="text"
+                  onChange={onAddEventFormChange}
+                />
+              </label>
+
+              <label htmlFor="name">
+                Event Name
+                <Input
+                  name="name"
+                  placeholder="eg, Berlin, Reykjavík, 2024"
+                  type="text"
+                  onChange={onAddEventFormChange}
+                />
+              </label>
+
+              <label htmlFor="start_date">
+                Start Date
+                <Input
+                  name="start_date"
+                  placeholder="Start Date"
+                  type="date"
+                  onChange={onAddEventFormChange}
+                />
+              </label>
+
+              <label htmlFor="end_date">
+                End Date
+                <Input
+                  name="end_date"
+                  placeholder="End Date"
+                  type="date"
+                  onChange={onAddEventFormChange}
+                />
+              </label>
+
+              <label htmlFor="location">
+                Location
+                <Input
+                  name="location"
+                  placeholder="Location"
+                  type="text"
+                  onChange={onAddEventFormChange}
+                />
+              </label>
+
+              <div className="flex w-full justify-between gap-4">
+                <Button className="w-full" color="danger" onClick={router.back}>
+                  Cancel
+                </Button>
+                <Button
+                  className="w-full"
+                  color="primary"
+                  formAction={handleAddEventFormSubmit}
+                  type="submit"
+                >
+                  Add
+                </Button>
+              </div>
+            </form>
+          </CardBody>
+        </Card>
+      </div>
+    </section>
+  );
+};
+
+export default AddEventPage;
