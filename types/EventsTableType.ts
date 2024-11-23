c73e78ac8b396f608eaba09fa9c93834406b4693
LeightonGuang <leighton.guang@icloud.com>
2024-11-23 12:26:02 +0000

    Fix: Start date, end date and prize pool can be empty

--- types/EventsTableType.ts
+++ types/EventsTableType.ts
@@ -3,8 +3,9 @@ export interface EventsTableType {
   type: string;
   name: string;
   event_icon_url: string;
+  start_date?: string;
+  end_date?: string;
+  prize_pool?: string;
   location: string;
-  start_date: string;
-  end_date: string;
   created_at: string;
 }
