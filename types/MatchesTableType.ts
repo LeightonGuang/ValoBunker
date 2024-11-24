16acae7c91d2ae2f58f5ec1c72e9aced709d10ed
LeightonGuang <leighton.guang@icloud.com>
2024-11-24 22:52:54 +0000

    Feat: New matches table type

---
+++ types/MatchesTableType.ts
@@ -0,0 +1,10 @@
+export interface MatchesTableType {
+  id: number;
+  date_time: string;
+  event_id: number;
+  team1_id: number;
+  team1_score: number;
+  team2_id: number;
+  team2_score: number;
+  created_at: string;
+}
