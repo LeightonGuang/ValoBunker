96f44ee87eba987681de67a4b215d1ef9502d6f7
LeightonGuang <leighton.guang@icloud.com>
2024-11-29 13:21:26 +0000

    Fix: Remove browser warning for textvalue

--- app/page.tsx
+++ app/page.tsx
@@ -140,6 +140,7 @@ export default function Home() {
                         width={32}
                       />
                     }
+                    textValue={eventObj.name}
                   />
                 );
               })}
