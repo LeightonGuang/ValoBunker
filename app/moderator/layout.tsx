ce25fe6c8e420a02b0009bf54a2cc76fe281384a
LeightonGuang <leighton.guang@icloud.com>
2024-11-30 18:12:43 +0000

    Feat: Redirect to home page if user is not authorised

--- app/moderator/layout.tsx
+++ app/moderator/layout.tsx
@@ -1,13 +1,87 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { User } from "@supabase/supabase-js";
+
+import { getSupabase } from "@/utils/supabase/client";
+
 export default function ModeratorLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  // TODO: Add role check for authorized users, redirect to homepage if user lacks required role
+  const router = useRouter();
+  const supabase = getSupabase();
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState<User | null>(null);
+  const [hasRole, setHasRole] = useState(false);
+
+  const fetchUser = async () => {
+    const { data, error } = await supabase.auth.getSession();
+
+    if (error) {
+      console.error(error);
+    }
+
+    if (!data.session?.user) {
+      setUser(null);
+
+      return;
+    }
+
+    setUser(data.session?.user);
+  };
+
+  const fetchUserRole = async () => {
+    if (!user) {
+      setIsLoading(false);
+
+      return;
+    }
+
+    const { data, error } = await supabase
+      .from("user_roles")
+      .select(`*`)
+      .eq("user_uid", user.id);
+
+    if (error) {
+      console.error(error);
+    }
+
+    if (data) {
+      console.log(data);
+      if (data[0].role === "admin" || data[0].role === "moderator") {
+        setHasRole(true);
+      }
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  useEffect(() => {
+    fetchUserRole();
+  }, [user]);
+
+  useEffect(() => {
+    console.log("isLoading, hasRole", isLoading, hasRole);
+    if (!isLoading && !hasRole) {
+      router.push("/");
+    }
+  }, [isLoading, hasRole]);
 
   return (
     <div className="flex w-full flex-col items-center justify-center gap-4">
-      <div className="w-full">{children}</div>
+      {!isLoading && hasRole ? (
+        <div className="w-full">{children}</div>
+      ) : !isLoading && !hasRole ? (
+        <div>Redirecting...</div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 }
