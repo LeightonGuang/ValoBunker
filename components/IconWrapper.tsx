b73e9b3c9778246774d18c3c8f49dd58abf48309
LeightonGuang <leighton.guang@icloud.com>
2024-12-06 02:51:56 +0000

    Feat: New component for icon wrapper

---
+++ components/IconWrapper.tsx
@@ -0,0 +1,18 @@
+import { cn } from "@nextui-org/react";
+
+export const IconWrapper = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => (
+  <div
+    className={cn(
+      className,
+      "flex h-7 w-7 items-center justify-center rounded-small",
+    )}
+  >
+    {children}
+  </div>
+);
