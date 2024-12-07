3400c60d3eeec6d5b8364a524039c453d7f2f6ec
LeightonGuang <leighton.guang@icloud.com>
2024-12-07 19:49:03 +0000

    Feat: Working delete news button

--- app/moderator/manage/news/page.tsx
+++ app/moderator/manage/news/page.tsx
@@ -9,12 +9,29 @@ import {
   TableHeader,
 } from "@nextui-org/table";
 import Link from "next/link";
-import { Image } from "@nextui-org/react";
+import {
+  Image,
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  useDisclosure,
+  DropdownTrigger,
+  Modal,
+  ModalContent,
+  ModalBody,
+  ModalFooter,
+  Breadcrumbs,
+  BreadcrumbItem,
+  Code,
+} from "@nextui-org/react";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 import { title } from "@/components/primitives";
 import { NewsTableType } from "@/types/NewsTableType";
 import { getSupabase } from "@/utils/supabase/client";
+import { EllipsisIcon } from "@/components/icons";
 
 const columnsHeader: { name: string; sortable: boolean }[] = [
   { name: "Title", sortable: true },
@@ -25,6 +42,9 @@ const columnsHeader: { name: string; sortable: boolean }[] = [
 ];
 
 const ManageNewsPage = () => {
+  const router = useRouter();
+  const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure();
+  const [newsToDelete, setNewsToDelete] = useState<NewsTableType | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const [newsData, setNewsData] = useState<NewsTableType[]>([]);
 
@@ -40,7 +60,6 @@ const ManageNewsPage = () => {
       if (error) {
         console.error(error);
       } else {
-        console.log(data);
         setNewsData(data);
       }
     } catch (error) {
@@ -50,17 +69,56 @@ const ManageNewsPage = () => {
     }
   };
 
+  const deleteNewsById = async (id: number) => {
+    try {
+      const supabase = getSupabase();
+      const { error } = await supabase.from("news").delete().eq("id", id);
+
+      if (error) {
+        console.error(error);
+      } else {
+        // console.log(data);
+        fetchAllNews();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const topContent = () => (
+    <div className="flex items-center justify-between">
+      <span className="text-small text-default-400">
+        Total {newsData.length} News
+      </span>
+      <Button
+        color="primary"
+        endContent={<span>+</span>}
+        onClick={() => router.push("/moderator/manage/news/create")}
+      >
+        News Article
+      </Button>
+    </div>
+  );
+
   useEffect(() => {
     fetchAllNews();
   }, []);
 
   return (
     <section>
+      <Breadcrumbs aria-label="News">
+        <BreadcrumbItem href="/moderator/manage">Manage</BreadcrumbItem>
+        <BreadcrumbItem>News</BreadcrumbItem>
+      </Breadcrumbs>
+
       <h1 className={title()}>Manage News</h1>
 
       <div>
-        <Link href="/moderator/manage/news/create">Create News</Link>
-        <Table aria-label="News">
+        <Table
+          aria-label="News"
+          topContent={topContent()}
+          topContentPlacement="outside"
+        >
           <TableHeader>
             {columnsHeader.map((column, i) => (
               <TableColumn key={i}>{column.name}</TableColumn>
@@ -70,6 +128,7 @@ const ManageNewsPage = () => {
             {newsData.map((news) => (
               <TableRow key={news.id}>
                 <TableCell>{news.title}</TableCell>
+
                 <TableCell>
                   <Image
                     alt={news.title}
@@ -77,20 +136,87 @@ const ManageNewsPage = () => {
                     src={news.img_url}
                   />
                 </TableCell>
+
                 <TableCell>
                   <div className="line-clamp-2 overflow-hidden text-ellipsis">
                     {news.content}
                   </div>
                 </TableCell>
+
                 <TableCell className="whitespace-nowrap">
                   {news.news_date}
                 </TableCell>
-                <TableCell>Ac</TableCell>
+
+                <TableCell>
+                  <Dropdown>
+                    <DropdownTrigger>
+                      <Button isIconOnly variant="light">
+                        <EllipsisIcon />
+                      </Button>
+                    </DropdownTrigger>
+                    <DropdownMenu>
+                      <DropdownItem
+                        onClick={() =>
+                          router.push(`/moderator/manage/news/edit/${news.id}`)
+                        }
+                      >
+                        Edit
+                      </DropdownItem>
+                      <DropdownItem
+                        onClick={() => {
+                          onOpen();
+                          setNewsToDelete(news);
+                        }}
+                      >
+                        Delete
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </Dropdown>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
         </Table>
       </div>
+
+      <Modal
+        isOpen={isOpen}
+        size="md"
+        onClose={onClose}
+        onOpenChange={onOpenChange}
+      >
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalBody className="text-medium">
+                {`Are you sure you want to delete News: `}
+                <Image
+                  className="h-16 min-h-16 w-16 min-w-16"
+                  src={newsToDelete?.img_url}
+                />
+                <span>
+                  <Code className="whitespace-break-spaces" color="default">
+                    {newsToDelete?.title}
+                  </Code>
+                  ?
+                </span>
+              </ModalBody>
+              <ModalFooter>
+                <Button onClick={onClose}>Cancel</Button>
+                <Button
+                  color="danger"
+                  onClick={() => {
+                    newsToDelete && deleteNewsById(newsToDelete?.id);
+                    onClose();
+                  }}
+                >
+                  Delete
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
     </section>
   );
 };
