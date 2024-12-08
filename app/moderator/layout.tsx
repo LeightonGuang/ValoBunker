2c666a3fa1770391fab376ff121b47ad6bd2e789
LeightonGuang <leighton.guang@icloud.com>
2024-12-08 22:59:11 +0000

    Feat: Listbox indicates which menu is selected

--- app/moderator/layout.tsx
+++ app/moderator/layout.tsx
@@ -1,27 +1,30 @@
 "use client";
 
+import { Key } from "@react-types/shared";
 import { useRouter } from "next/navigation";
-import { useEffect, useState, useCallback } from "react";
 import { Listbox, ListboxItem } from "@nextui-org/listbox";
+import React, { useEffect, useState, useCallback } from "react";
 
 import { TeamIcon, UserIcon } from "@/components/icons";
 import { getSupabase } from "@/utils/supabase/client";
 import { IconWrapper } from "@/components/IconWrapper";
 
 const menuList = [
-  { name: "Agents", href: "/moderator/manage/agents" },
-  { name: "Events", href: "/moderator/manage/events" },
-  { name: "News", href: "/moderator/manage/news" },
-  { name: "Patches", href: "/moderator/manage/patches" },
-  { name: "Players", href: "/moderator/manage/players" },
+  { name: "Agents", key: "agents", href: "/moderator/manage/agents" },
+  { name: "Events", key: "events", href: "/moderator/manage/events" },
+  { name: "News", key: "news", href: "/moderator/manage/news" },
+  { name: "Patches", key: "patches", href: "/moderator/manage/patches" },
+  { name: "Players", key: "players", href: "/moderator/manage/players" },
   {
     name: "Teams",
+    key: "teams",
     href: "/moderator/manage/teams",
     icon: <TeamIcon />,
     iconClassName: "bg-default/50 text-foreground",
   },
   {
     name: "Users",
+    key: "users",
     href: "/moderator/manage/users",
     icon: <UserIcon />,
     iconClassName: "bg-warning/10 text-warning",
@@ -36,8 +39,11 @@ export default function ModeratorLayout({
   const router = useRouter();
   const supabase = getSupabase();
 
-  const [hasRole, setHasRole] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
+  const [hasRole, setHasRole] = useState(false);
+  const [selectedKeys, setSelectedKeys] = useState<
+    "all" | Iterable<Key> | undefined
+  >(["agents"]);
 
   const fetchUserAndRole = useCallback(async () => {
     try {
@@ -97,15 +103,29 @@ export default function ModeratorLayout({
       ) : hasRole ? (
         <div className="w-full">
           <div className="flex flex-col gap-4 lg:flex-row">
-            <div className="flex h-[80dvh] w-full items-center lg:w-48">
+            <div className="flex w-full items-center lg:h-[80dvh] lg:w-48">
               <Listbox
+                disallowEmptySelection
                 aria-label="Manage"
                 className="gap-0 overflow-visible rounded-medium bg-content1 shadow-small dark:divide-default-100/80"
-                onAction={(key) => router.push(menuList[key as number].href)}
+                selectedKeys={selectedKeys}
+                selectionMode="single"
+                onSelectionChange={(keys) => {
+                  setSelectedKeys(keys);
+
+                  const selectedKey = Array.from(keys)[0] as string;
+                  const selectedItem = menuList.find(
+                    (item) => item.key === selectedKey,
+                  );
+
+                  if (selectedItem) {
+                    router.push(selectedItem.href);
+                  }
+                }}
               >
-                {menuList.map((button, i) => (
+                {menuList.map((button) => (
                   <ListboxItem
-                    key={i}
+                    key={button.key}
                     startContent={
                       <IconWrapper className={button.iconClassName}>
                         {button.icon}
@@ -117,7 +137,9 @@ export default function ModeratorLayout({
                 ))}
               </Listbox>
             </div>
-            <div className="flex w-full justify-center">{children}</div>
+            <div className="lg:flex lg:w-full lg:justify-center">
+              {children}
+            </div>
           </div>
         </div>
       ) : null}
