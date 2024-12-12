d5e453b4c550098f90531828586d170fb2aa6755
LeightonGuang <leighton.guang@icloud.com>
2024-12-12 17:12:08 +0000

    Feat: New sign up with google function

---
+++ utils/signUpWithGoogle.ts
@@ -0,0 +1,28 @@
+"use client";
+
+import { getSupabase } from "./supabase/client";
+
+export async function signUpWithGoogle() {
+  try {
+    const supabase = getSupabase();
+
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${process.env.NEXT_PUBLIC_APP_URL}/auth/callback`, // Dynamic redirect URL
+        queryParams: {
+          access_type: "offline",
+          prompt: "consent",
+        },
+      },
+    });
+
+    if (error) {
+      console.error(error);
+      throw error;
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
