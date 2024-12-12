f2c19b9cd89e7021506c6a938e9864cff3a17fe1
LeightonGuang <leighton.guang@icloud.com>
2024-12-12 19:57:01 +0000

    Feat: New AdSense ad banner component

---
+++ components/AdSenseBanner.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useEffect } from "react";
+
+interface AdSenseBannerProps {
+  dataAdSlot: string;
+  dataAdFormat: string;
+  dataFullWidthResponsive: boolean;
+}
+
+const AdSenseBanner = ({
+  dataAdSlot,
+  dataAdFormat,
+  dataFullWidthResponsive,
+}: AdSenseBannerProps) => {
+  useEffect(() => {
+    try {
+      ((window as any).adsbygoogle = (window as any).adsbygoogle || []).push(
+        {},
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }, []);
+
+  return (
+    <ins
+      className="adsbygoogle"
+      data-ad-client={process.env.NextUI_ADSENSE_CLIENT_ID}
+      data-ad-format={dataAdFormat}
+      data-ad-slot={dataAdSlot}
+      data-full-width-responsive={dataFullWidthResponsive.toString()}
+      style={{ display: "block" }}
+    >
+      {" "}
+    </ins>
+  );
+};
+
+export default AdSenseBanner;
