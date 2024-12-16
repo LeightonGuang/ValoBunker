f39ed868e6b6f6c1ca5512b593be7195c1a07d74
LeightonGuang <leighton.guang@icloud.com>
2024-12-16 03:03:58 +0000

    Feat: Use rich text editor for create content for news

--- app/moderator/manage/news/create/page.tsx
+++ app/moderator/manage/news/create/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { Input, Textarea } from "@nextui-org/input";
+import { Input } from "@nextui-org/input";
 import { Button, Card, CardBody, CardHeader, Image } from "@nextui-org/react";
 
+import { title } from "@/components/primitives";
 import { getSupabase } from "@/utils/supabase/client";
 import { NewsTableType } from "@/types/NewsTableType";
+import NewsTextEditor from "@/components/NewsTextEditor";
+
 const CreateNewsPage = () => {
   const router = useRouter();
+
   const [newsForm, setNewsForm] = useState<NewsTableType>({
     img_url: "",
     title: "",
@@ -49,67 +53,83 @@ const CreateNewsPage = () => {
     }
   };
 
+  useEffect(() => {
+    console.log(newsForm.content);
+  }, [newsForm.content]);
+
   return (
-    <section>
-      <div>
-        <Card className="w-96">
-          <CardHeader>Create News</CardHeader>
-          <CardBody>
-            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-              <div className="flex flex-col items-center gap-4">
-                <Image alt={newsForm.title} src={newsForm.img_url} />
-                <Input
-                  label="Image URL"
-                  name="img_url"
-                  placeholder="Image URL"
-                  type="url"
-                  value={newsForm.img_url}
-                  onChange={onNewsFormChange}
-                />
+    <section className="flex w-full items-center justify-center">
+      <Card className="w-full">
+        <CardHeader className={title()}>Create News</CardHeader>
+        <CardBody>
+          <form className="flex flex-col" onSubmit={handleSubmit}>
+            <div className="flex flex-col gap-4">
+              <div className="flex flex-col gap-4 lg:flex-row">
+                <div className="aspect-w-16 aspect-h-9 flex items-center justify-center lg:w-1/2">
+                  <Image
+                    alt={newsForm.title}
+                    className="object-cover"
+                    classNames={{
+                      wrapper: "w-full h-full",
+                    }}
+                    src={
+                      newsForm.img_url === ""
+                        ? "https://via.placeholder.com/1920x1080"
+                        : newsForm.img_url
+                    }
+                    style={{ objectFit: "contain" }}
+                  />
+                </div>
+
+                <div className="flex flex-col gap-4 lg:w-1/2">
+                  <Input
+                    label="Image URL"
+                    name="img_url"
+                    placeholder="Image URL"
+                    type="url"
+                    value={newsForm.img_url}
+                    onChange={onNewsFormChange}
+                  />
+
+                  <Input
+                    isRequired
+                    errorMessage="Please enter a title"
+                    isInvalid={newsFormErrors.title}
+                    label="Title"
+                    name="title"
+                    placeholder="Title"
+                    type="text"
+                    value={newsForm.title}
+                    onChange={onNewsFormChange}
+                  />
+
+                  <Input
+                    isRequired
+                    errorMessage="Please enter the news date"
+                    isInvalid={newsFormErrors.news_date}
+                    label="News Date"
+                    name="news_date"
+                    type="date"
+                    value={newsForm.news_date}
+                    onChange={onNewsFormChange}
+                  />
+                </div>
               </div>
 
-              <Input
-                isRequired
-                errorMessage="Please enter a title"
-                isInvalid={newsFormErrors.title}
-                label="Title"
-                name="title"
-                placeholder="Title"
-                type="text"
-                value={newsForm.title}
-                onChange={onNewsFormChange}
+              <NewsTextEditor
+                content={newsForm.content}
+                onContentChange={(content) =>
+                  setNewsForm({ ...newsForm, content })
+                }
               />
+            </div>
 
-              <Textarea
-                isRequired
-                errorMessage="Please enter some content"
-                isInvalid={newsFormErrors.content}
-                label="Content"
-                name="content"
-                placeholder="Content"
-                type="textarea"
-                value={newsForm.content}
-                onChange={onNewsFormChange}
-              />
-
-              <Input
-                isRequired
-                errorMessage="Please enter the news date"
-                isInvalid={newsFormErrors.news_date}
-                label="News Date"
-                name="news_date"
-                type="date"
-                value={newsForm.news_date}
-                onChange={onNewsFormChange}
-              />
-
-              <Button color="primary" type="submit">
-                Create
-              </Button>
-            </form>
-          </CardBody>
-        </Card>
-      </div>
+            <Button className="mt-4" color="primary" type="submit">
+              Create
+            </Button>
+          </form>
+        </CardBody>
+      </Card>
     </section>
   );
 };
