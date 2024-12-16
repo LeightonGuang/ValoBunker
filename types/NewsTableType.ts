f1c99c9880fc735b8d74f21e4e162691f8718160
LeightonGuang <leighton.guang@icloud.com>
2024-12-16 07:31:46 +0000

    Fix: Description could be empty

--- types/NewsTableType.ts
+++ types/NewsTableType.ts
@@ -2,7 +2,7 @@ export interface NewsTableType {
   id: number;
   img_url?: string;
   headline: string;
-  description: string;
+  description?: string;
   content: string;
   news_date: string;
   link_url?: string;
