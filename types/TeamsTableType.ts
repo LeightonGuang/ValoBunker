1970102a31d832b0d964cc1ce0a22dfb7f9fdb8a
LeightonGuang <leighton.guang@icloud.com>
2024-12-18 02:44:10 +0000

    Feat: Replace vct_league as a foreign key

--- types/TeamsTableType.ts
+++ types/TeamsTableType.ts
@@ -1,12 +1,13 @@
 import { CoachesTableType } from "./CoachesTableType";
 import { PlayersTableType } from "./PlayersTableType";
+import { VctLeaguesTableType } from "./VctLeaguesTableType";
 
 export interface TeamsTableType {
   id: number;
   name: string;
   logo_url: string;
   country: string;
-  vct_league: string;
+  vct_league: VctLeaguesTableType;
   tag: string;
   created_at: string;
   players: PlayersTableType[];
