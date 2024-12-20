a4675b7fda8e011211266dd3e0ea1f856735d184
LeightonGuang <leighton.guang@icloud.com>
2024-12-20 02:19:23 +0000

    Feat: New filter by league feature and player role rework

--- app/moderator/manage/players/page.tsx
+++ app/moderator/manage/players/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
-import { useEffect, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import {
+  Chip,
   User,
   Image,
   Table,
+  Input,
   Button,
   Tooltip,
   Dropdown,
@@ -27,21 +29,23 @@ import {
 import { title } from "@/components/primitives";
 import { getSupabase } from "@/utils/supabase/client";
 import { PlayersTableType } from "@/types/PlayersTableType";
-import { ChevronDown } from "@/components/icons";
+import { ChevronDown, SearchIcon } from "@/components/icons";
 import { VctLeaguesTableType } from "@/types/VctLeaguesTableType";
+import { RolesTableType } from "@/types/RolesTableType";
 
 const ManagePlayersPage = () => {
   const router = useRouter();
-  const [page, setPage] = useState(1);
-  const [isLoading, setIsLoading] = useState(true);
   const [playersData, setPlayersData] = useState<PlayersTableType[]>([]);
+  const [searchFilterValue, setSearchFilterValue] = useState("");
+  const [leagueFilter, setLeagueFilter] = useState<Selection>("all");
+  const [isLoading, setIsLoading] = useState(true);
   const [vctLeagues, setVctLeagues] = useState<VctLeaguesTableType[]>([]);
   const [sortDescriptor, setSortDescriptor] = useState<SortDescriptor>({
     column: "ign",
     direction: "ascending",
   });
-  const [leagueFilter, setLeagueFilter] = useState<Selection>("all");
-  const [searchFilterValue, setSearchFilterValue] = useState("");
+  const [page, setPage] = useState(1);
+  const [roleData, setRoleData] = useState<RolesTableType[]>([]);
 
   const rowsPerPage = 10;
 
@@ -108,20 +112,24 @@ const ManagePlayersPage = () => {
     const end = start + rowsPerPage;
 
     return sortedPlayers.slice(start, end);
-  }, [page, sortedPlayers]);
+  }, [page, filteredItems, sortedPlayers]);
 
-  const fetchPlayers = async () => {
+  const fetchData = async () => {
     try {
       const supabase = getSupabase();
-      const { data: playersData, error: playersError }: any =
-        await supabase.from("players").select(`
+      const { data: playersData, error: playersError }: any = await supabase
+        .from("players")
+        .select(
+          `
         *, teams(
           *, vct_league(
             *
             )
           )
         )
-      `);
+      `,
+        )
+        .order("ign", { ascending: true });
 
       if (playersError) {
         console.error(playersError);
@@ -131,12 +139,24 @@ const ManagePlayersPage = () => {
 
       const { data: vctLeaguesData, error: vctLeaguesError } = await supabase
         .from("vct_leagues")
-        .select("*");
+        .select("*")
+        .order("id", { ascending: true });
 
       if (vctLeaguesError) {
         console.error(vctLeaguesError);
       } else {
         setVctLeagues(vctLeaguesData);
+      }
+
+      const { data: roleData, error: roleError } = await supabase
+        .from("roles")
+        .select("*")
+        .order("id", { ascending: true });
+
+      if (roleError) {
+        console.error(roleError);
+      } else {
+        setRoleData(roleData);
       }
     } catch (error) {
       console.error(error);
@@ -161,58 +181,98 @@ const ManagePlayersPage = () => {
     return age;
   };
 
-  const topContent = () => {
+  const onSearchChange = useCallback((ign?: string) => {
+    if (ign) {
+      setSearchFilterValue(ign);
+      setPage(1);
+    } else {
+      setSearchFilterValue("");
+    }
+  }, []);
+
+  const topContent = useMemo(() => {
+    const searchInput = (
+      <Input
+        isClearable
+        className="w-full sm:max-w-[44%]"
+        placeholder="Search by IGN"
+        startContent={<SearchIcon />}
+        value={searchFilterValue}
+        onClear={() => {
+          setSearchFilterValue("");
+          setPage(1);
+        }}
+        onValueChange={onSearchChange}
+      />
+    );
+
+    const leagueDropdown = (
+      <Dropdown>
+        <DropdownTrigger>
+          <Button endContent={<ChevronDown fill="currentColor" />}>
+            League
+          </Button>
+        </DropdownTrigger>
+        <DropdownMenu
+          disallowEmptySelection
+          aria-label="League"
+          closeOnSelect={false}
+          selectedKeys={leagueFilter}
+          selectionMode="multiple"
+          onSelectionChange={setLeagueFilter}
+        >
+          {vctLeagues.map((league) => (
+            <DropdownItem
+              key={league.id}
+              aria-selected="false"
+              textValue={league.name}
+            >
+              <User
+                avatarProps={{
+                  isBordered: false,
+                  size: "sm",
+                  src: league.logo_url,
+                  className: "bg-transparent rounded-none",
+                }}
+                name={league.name}
+              />
+            </DropdownItem>
+          ))}
+        </DropdownMenu>
+      </Dropdown>
+    );
+
+    const addButton = (
+      <Button
+        color="primary"
+        endContent={<span>+</span>}
+        onPress={() => router.push("/moderator/manage/players/add")}
+      >
+        Player
+      </Button>
+    );
+
     return (
       <div className="flex justify-between">
         <span className="flex items-center text-small text-default-400">
           Total {playersData.length} players
         </span>
 
-        <div className="flex gap-2">
-          <Dropdown>
-            <DropdownTrigger>
-              <Button endContent={<ChevronDown fill="currentColor" />}>
-                League
-              </Button>
-            </DropdownTrigger>
-            <DropdownMenu
-              disallowEmptySelection
-              aria-label="League"
-              closeOnSelect={false}
-              selectedKeys={leagueFilter}
-              selectionMode="multiple"
-              onSelectionChange={setLeagueFilter}
-            >
-              {vctLeagues.map((league) => (
-                <DropdownItem
-                  key={league.id}
-                  aria-selected="false"
-                  textValue={league.name}
-                >
-                  <User
-                    avatarProps={{
-                      isBordered: false,
-                      size: "sm",
-                      src: league.logo_url,
-                      className: "bg-transparent rounded-none",
-                    }}
-                    name={league.name}
-                  />
-                </DropdownItem>
-              ))}
-            </DropdownMenu>
-          </Dropdown>
-          <Button
-            color="primary"
-            endContent={<span>+</span>}
-            onPress={() => router.push("/moderator/manage/players/add")}
-          >
-            Player
-          </Button>
+        <div className="flex w-fit justify-end gap-2">
+          {searchInput}
+          {leagueDropdown}
+          {addButton}
         </div>
       </div>
     );
-  };
+  }, [
+    playersData,
+    searchFilterValue,
+    vctLeagues,
+    leagueFilter,
+    onSearchChange,
+    router,
+  ]);
 
   const onSortChange = (descriptor: SortDescriptor) => {
     setSortDescriptor(descriptor);
@@ -223,11 +283,15 @@ const ManagePlayersPage = () => {
       { name: "Name", sortBy: "ign", sortable: true },
       { name: "Country", sortBy: "country", sortable: true },
       { name: "Team", sortBy: "teams.name", sortable: true },
-      { name: "Role", sortBy: "role", sortable: true },
+      { name: "Role", sortBy: "role", sortable: false },
       { name: "Age", sortBy: "age", sortable: true },
       { name: "League", sortBy: "teams.vct_league.name", sortable: true },
       { name: "Actions", sortBy: "actions", sortable: false },
     ];
+
+    useEffect(() => {
+      console.log(playersData);
+    }, [playersData]);
 
     return (
       <Table
@@ -237,18 +301,17 @@ const ManagePlayersPage = () => {
             <Pagination
               isCompact
               showControls
-              showShadow
               color="primary"
               page={page}
               total={pages}
-              onChange={(page) => setPage(page)}
+              onChange={setPage}
             />
           </div>
         }
         className="w-full"
         selectionMode="single"
         sortDescriptor={sortDescriptor}
-        topContent={topContent()}
+        topContent={topContent}
         topContentPlacement="outside"
         onSortChange={onSortChange}
       >
@@ -260,6 +323,7 @@ const ManagePlayersPage = () => {
           ))}
         </TableHeader>
         <TableBody
+          emptyContent="No players found"
           isLoading={isLoading}
           items={isLoading ? [] : paginatedPlayers}
         >
@@ -284,7 +348,49 @@ const ManagePlayersPage = () => {
                 </Tooltip>
               </TableCell>
 
-              <TableCell>{item.role}</TableCell>
+              <TableCell>
+                <div className="flex gap-2">
+                  {item.roles.map((role) => (
+                    <Chip
+                      key={
+                        role === "1"
+                          ? "Controller"
+                          : role === "2"
+                            ? "Duelist"
+                            : role === "3"
+                              ? "Initiator"
+                              : role === "4"
+                                ? "Sentinel"
+                                : role
+                      }
+                      color={
+                        role === "1"
+                          ? "success"
+                          : role === "2"
+                            ? "danger"
+                            : role === "3"
+                              ? "warning"
+                              : role === "4"
+                                ? "secondary"
+                                : role === "IGL"
+                                  ? "primary"
+                                  : "default"
+                      }
+                      variant="flat"
+                    >
+                      {role === "1"
+                        ? "Controller"
+                        : role === "2"
+                          ? "Duelist"
+                          : role === "3"
+                            ? "Initiator"
+                            : role === "4"
+                              ? "Sentinel"
+                              : role}
+                    </Chip>
+                  ))}
+                </div>
+              </TableCell>
 
               <TableCell>
                 {item.birthday ? convertedAge(item.birthday) : "-"}
@@ -316,7 +422,7 @@ const ManagePlayersPage = () => {
   };
 
   useEffect(() => {
-    fetchPlayers();
+    fetchData();
   }, []);
 
   return (
