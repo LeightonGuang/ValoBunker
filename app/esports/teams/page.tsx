71dc01bd9fc87de0156c70ea97085ea1d77a7bcc
LeightonGuang <leighton.guang@icloud.com>
2024-12-30 11:23:17 +0000

    Feat: New teams accordion design and working taps for different regions

--- app/esports/teams/page.tsx
+++ app/esports/teams/page.tsx
@@ -1,60 +1,77 @@
 "use client";
 
 import {
-  Accordion,
-  AccordionItem,
-  Divider,
-  Image,
   Tab,
+  Card,
+  Chip,
   Tabs,
-  User,
+  Image,
+  CardBody,
+  Accordion,
+  CardFooter,
+  AccordionItem,
 } from "@nextui-org/react";
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { title } from "@/components/primitives";
 import { getSupabase } from "@/utils/supabase/client";
 import { TeamsTableType } from "@/types/TeamsTableType";
+import { RolesTableType } from "@/types/RolesTableType";
 
-const regionTabs: { region: string; region_icon_url: string }[] = [
+const regionTabs: { name: string; region_icon_url: string }[] = [
   {
-    region: "All",
+    name: "All",
     region_icon_url: "https://cdn3.emoji.gg/emojis/7053-vct-red-logo.png",
   },
   {
-    region: "Americas",
+    name: "Americas",
     region_icon_url: "https://owcdn.net/img/640f5ab71dfbb.png",
   },
   {
-    region: "China",
+    name: "China",
     region_icon_url: "https://owcdn.net/img/65dd97cea9a25.png",
   },
   {
-    region: "EMEA",
+    name: "EMEA",
     region_icon_url: "https://owcdn.net/img/65ab54a77831c.png",
   },
   {
-    region: "Pacific",
+    name: "Pacific",
     region_icon_url: "https://owcdn.net/img/640f5ae002674.png",
   },
 ];
 
 const TeamsPage = () => {
   const [isLoading, setIsLoading] = useState(true);
-  const [regionTab, setRegionTab] = useState("All");
+  const [selectedRegionTab, setSelectedRegionTab] = useState("All");
   const [teamsDataList, setTeamsDataList] = useState<TeamsTableType[]>([]);
+  const [rolesDataList, setRolesDataList] = useState<RolesTableType[]>([]);
+  const [filteredTeamList, setFilteredTeamList] = useState<TeamsTableType[]>(
+    [],
+  );
 
-  const getAllTeams = async () => {
+  const fetchData = async () => {
     try {
       const supabase = getSupabase();
-      const { data, error } = await supabase
+      const { data: teamsData, error: teamsError } = await supabase
         .from("teams")
-        .select(`*, players(*), coaches(*)`)
+        .select(`*, players(*), coaches(*), vct_league(*)`)
         .order("name", { ascending: true });
 
-      if (error) {
-        console.error(error);
+      if (teamsError) {
+        console.error(teamsError);
       } else {
-        setTeamsDataList(data);
+        setTeamsDataList(teamsData);
+      }
+
+      const { data: rolesData, error: rolesError } = await supabase
+        .from("roles")
+        .select("*");
+
+      if (rolesError) {
+        console.error(rolesError);
+      } else {
+        setRolesDataList(rolesData);
       }
     } catch (error) {
       console.error(error);
@@ -63,17 +80,20 @@ const TeamsPage = () => {
     }
   };
 
-  const filteredTeamList = useMemo(() => {
-    return [...teamsDataList].filter((team) => {
-      if (regionTab === "All") return true;
-
-      return team.vct_league === regionTab;
-    });
-  }, [regionTab, teamsDataList]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   useEffect(() => {
-    getAllTeams();
-  }, []);
+    const teams = [...teamsDataList].filter((team) => {
+      if (selectedRegionTab === "All") return true;
+
+      return team.vct_league.name === selectedRegionTab;
+    });
+
+    setFilteredTeamList(teams);
+    console.log(teams);
+  }, [selectedRegionTab, teamsDataList]);
 
   return (
     <section>
@@ -81,99 +101,135 @@ const TeamsPage = () => {
         <h1 className={title()}>Teams</h1>
       </div>
       <div>
-        <Tabs
-          className="mt-4 flex justify-center"
-          classNames={{ tabList: "gap-0 lg:gap-2", tab: "max-w-fit h-max" }}
-          onSelectionChange={(key) => {
-            setRegionTab(String(key));
-          }}
-        >
-          {regionTabs.map((tab, i) => (
-            <Tab
-              key={regionTabs[i].region}
-              title={
-                <div className="flex flex-col items-center gap-2 lg:flex-row">
-                  <Image
-                    alt={tab.region}
-                    className="h-4 w-4"
-                    height={16}
-                    src={tab.region_icon_url}
-                    width={16}
-                  />
-                  {tab.region}
-                </div>
-              }
-            >
-              <Accordion
-                className="mt-4 flex flex-col gap-4"
-                showDivider={false}
+        {!isLoading && (
+          <Tabs
+            className="mt-4 flex justify-center"
+            classNames={{ tabList: "gap-0 lg:gap-2", tab: "max-w-fit h-max" }}
+            items={regionTabs}
+            selectedKey={selectedRegionTab}
+            onSelectionChange={(key) => {
+              setSelectedRegionTab(String(key));
+            }}
+          >
+            {regionTabs.map((region) => (
+              <Tab
+                key={region.name}
+                title={
+                  <div className="flex flex-col items-center gap-2 lg:flex-row">
+                    <Image
+                      alt={region.name}
+                      className="h-4 w-4"
+                      height={16}
+                      src={region.region_icon_url}
+                      width={16}
+                    />
+                    {region.name}
+                  </div>
+                }
               >
-                {filteredTeamList.map((teamObj) => (
-                  <AccordionItem
-                    key={teamObj.id}
-                    aria-label={teamObj.name}
-                    startContent={
-                      <Image
-                        classNames={{ img: "rounded-none" }}
-                        height={48}
-                        src={teamObj.logo_url}
-                        width={48}
-                      />
-                    }
-                    title={teamObj.name}
-                    variant="splitted"
-                  >
-                    <div>
-                      <h2>Players</h2>
-                      <ul className="mt-4 flex flex-col gap-4 lg:flex-row lg:flex-wrap">
+                <Accordion
+                  key={selectedRegionTab}
+                  className="mt-4 flex flex-col gap-4 p-0"
+                  selectionMode="multiple"
+                  showDivider={false}
+                >
+                  {filteredTeamList.map((teamObj) => (
+                    <AccordionItem
+                      key={teamObj.id}
+                      aria-label={teamObj.name}
+                      startContent={
+                        <Image
+                          classNames={{ img: "rounded-none" }}
+                          height={48}
+                          src={teamObj.logo_url}
+                          width={48}
+                        />
+                      }
+                      title={teamObj.name}
+                      variant="splitted"
+                    >
+                      <ul className="grid grid-cols-2 gap-4 lg:grid-cols-5">
                         {[...teamObj.players]
                           .sort((a, b) =>
-                            a.role === "IGL" ? -1 : b.role === "IGL" ? 1 : 0,
+                            a.roles[0] === "IGL"
+                              ? -1
+                              : b.roles[0] === "IGL"
+                                ? 1
+                                : 0,
                           )
                           .map((player) => (
                             <li key={player.id}>
-                              <User
-                                avatarProps={{
-                                  src: player.profile_picture_url,
-                                  isBordered: player.role === "IGL",
-                                  color:
-                                    player.role === "IGL"
-                                      ? "primary"
-                                      : "default",
-                                  classNames: {
-                                    base: "bg-white",
-                                  },
-                                }}
-                                className="lg:p-2"
-                                description={
+                              <Card>
+                                <CardBody className="flex justify-center">
+                                  <Image
+                                    className="aspect-square h-full w-full bg-white object-cover object-top"
+                                    src={
+                                      player.profile_picture_url
+                                        ? player.profile_picture_url
+                                        : "https://placehold.co/500x500"
+                                    }
+                                  />
+                                </CardBody>
+                                <CardFooter className="flex text-large">
                                   <div className="flex flex-col">
-                                    <span className="whitespace-nowrap">
-                                      {player.name}
-                                    </span>
-                                    <span>{player.role}</span>
+                                    <span>{player.ign}</span>
+
+                                    <div className="w-full">
+                                      <span className="text-small text-default-400">
+                                        {player.roles.length === 1
+                                          ? "Role:"
+                                          : "Roles:"}
+                                      </span>
+
+                                      <ul className="flex w-full flex-wrap gap-x-2">
+                                        {player.roles.map((roleId) => {
+                                          return (
+                                            <li key={roleId}>
+                                              <Chip
+                                                color={
+                                                  roleId === "1"
+                                                    ? "success"
+                                                    : roleId === "2"
+                                                      ? "danger"
+                                                      : roleId === "3"
+                                                        ? "warning"
+                                                        : roleId === "4"
+                                                          ? "secondary"
+                                                          : roleId === "IGL"
+                                                            ? "primary"
+                                                            : "default"
+                                                }
+                                                size="sm"
+                                              >
+                                                {roleId === "1" ||
+                                                roleId === "2" ||
+                                                roleId === "3" ||
+                                                roleId === "4"
+                                                  ? rolesDataList.filter(
+                                                      (roleObj) =>
+                                                        roleObj.id ===
+                                                        Number(roleId),
+                                                    )[0].name
+                                                  : roleId}
+                                              </Chip>
+                                            </li>
+                                          );
+                                        })}
+                                      </ul>
+                                    </div>
                                   </div>
-                                }
-                                name={player.ign}
-                              />
+                                </CardFooter>
+                              </Card>
                             </li>
                           ))}
                       </ul>
-                      <Divider className="my-4" />
-                      <h2>Coach</h2>
-                      <div className="mt-4">
-                        {teamObj.coaches.length === 0 ? (
-                          <User name="N/A" />
-                        ) : (
-                          <User name={teamObj.coaches[0].name} />
-                        )}
-                      </div>
-                    </div>
-                  </AccordionItem>
-                ))}
-              </Accordion>
-            </Tab>
-          ))}
-        </Tabs>
+                    </AccordionItem>
+                  ))}
+                </Accordion>
+              </Tab>
+            ))}
+          </Tabs>
+        )}
       </div>
     </section>
   );
