565f54ecd42cf667c4019ab31c2549ec8864e293
LeightonGuang <leighton.guang@icloud.com>
2025-01-13 13:56:05 +0000

    Feat: Set tungsten as default font

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
+        sans: ["tungsten", "var(--font-sans)"],
+        mono: ["tungsten", "var(--font-mono)"],
       },
     },
     colors: {
