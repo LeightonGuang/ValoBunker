925fe976e87ac5c9d5fc2db6c2572e9be384895c
LeightonGuang <leighton.guang@icloud.com>
2025-01-14 19:04:18 +0000

    Feat: New type for maps database table

---
+++ types/MapsTableType.ts
@@ -0,0 +1,11 @@
+export interface MapsTableType {
+  id: number;
+  name: string;
+  cover_img_url: string;
+  mini_map_url: string;
+  location: string;
+  competitive_map_pool: boolean;
+  site_count: number;
+  release_date: Date;
+  created_at: Date;
+}
