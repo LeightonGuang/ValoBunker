ae6570a2cdd42a9ec3a9608ccba4545079d7b9bf
LeightonGuang <leighton.guang@icloud.com>
2025-01-16 13:50:08 +0000

    Fix: Image size bug on mobile view

--- components/CountdownCard.tsx
+++ components/CountdownCard.tsx
@@ -83,7 +83,7 @@ const CountdownCard = ({
             startContent={
               <Image
                 alt={event.name}
-                className="h-8 w-full rounded-md object-cover"
+                className="h-8 rounded-md object-cover"
                 src={event.img_url}
               />
             }
