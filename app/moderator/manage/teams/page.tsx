a4e0f89782952059e2528f9c2e8f1569dae45938
LeightonGuang <leighton.guang@icloud.com>
2025-01-16 05:34:27 +0000

    Feat: New manage teams page

---
+++ app/moderator/manage/teams/page.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+  User,
+  Image,
+  Table,
+  Button,
+  Tooltip,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableColumn,
+  TableHeader,
+} from "@nextui-org/react";
+import { useRouter } from "next/navigation";
+
+import { title } from "@/components/primitives";
+import { getSupabase } from "@/utils/supabase/client";
+import { TeamsTableType } from "@/types/TeamsTableType";
+
+const ManageTeamsPage = () => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+  const [teams, setTeams] = useState<TeamsTableType[]>([]);
+  const fetchData = async () => {
+    try {
+      const supabase = getSupabase();
+      const { data: teamsData, error: teamsError } = await supabase
+        .from("teams")
+        .select("*, vct_league(*)")
+        .order("name", { ascending: true });
+
+      if (teamsError) {
+        console.error(teamsError);
+      } else {
+        setTeams(teamsData);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const TeamsTable = () => {
+    const columns = ["Team", "League", "country"];
+
+    const topContent = (
+      <div className="flex items-center justify-between">
+        <div className="text-small text-default-400">
+          Total {teams.length} Teams
+        </div>
+
+        <Button color="primary" startContent={<span>+</span>}>
+          Add Team
+        </Button>
+      </div>
+    );
+
+    return (
+      <Table
+        aria-label="Teams"
+        selectionMode="single"
+        topContent={topContent}
+        topContentPlacement="outside"
+        onRowAction={(key) => router.push(`/esports/teams/${key}`)}
+      >
+        <TableHeader>
+          {columns.map((column, i) => (
+            <TableColumn key={i}>{column}</TableColumn>
+          ))}
+        </TableHeader>
+
+        <TableBody isLoading={isLoading}>
+          {teams.map((team) => (
+            <TableRow key={team.id} className="cursor-pointer">
+              <TableCell>
+                {
+                  <User
+                    avatarProps={{
+                      src: team.logo_url,
+                      classNames: {
+                        base: "bg-transparent rounded-none",
+                      },
+                    }}
+                    name={team.name}
+                  />
+                }
+              </TableCell>
+
+              <TableCell>
+                <Tooltip content={team.vct_league.name}>
+                  <Image className="h-8 w-8" src={team.vct_league.logo_url} />
+                </Tooltip>
+              </TableCell>
+
+              <TableCell>{team.country}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <section className="w-full lg:mr-4">
+      <h1 className={title()}>Manage Teams</h1>
+      <div className="mt-6 w-full">
+        <TeamsTable />
+      </div>
+    </section>
+  );
+};
+
+export default ManageTeamsPage;
