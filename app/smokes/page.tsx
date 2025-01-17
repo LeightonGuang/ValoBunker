e2c8bdd25a43ff41234df2ed48eea2575a62dec6
LeightonGuang <leighton.guang@icloud.com>
2025-01-17 03:28:14 +0000

    Feat: Get smokes abilities from supabase

--- app/smokes/page.tsx
+++ app/smokes/page.tsx
@@ -1,18 +1,47 @@
 "use client";
 
-import Image from "next/image";
+import { useEffect, useState } from "react";
 import {
   Table,
+  TableRow,
   TableBody,
   TableCell,
   TableColumn,
   TableHeader,
-  TableRow,
 } from "@nextui-org/table";
+import Image from "next/image";
 import { Tooltip, User } from "@nextui-org/react";
 
-import smokesData from "@/public/data/smokesData.json";
 import { title } from "@/components/primitives";
+import { getSupabase } from "@/utils/supabase/client";
+
+interface CircularSmokeType {
+  id: number;
+  name: string;
+  icon_url: string;
+  duration: number;
+  radius: number;
+  cost: number;
+  regen?: number;
+  agents: {
+    name: string;
+    icon_url: string;
+  };
+}
+
+interface WallSmokeType {
+  id: number;
+  name: string;
+  icon_url: string;
+  duration: number;
+  length: number;
+  cost: number;
+  regen?: number;
+  agents: {
+    name: string;
+    icon_url: string;
+  };
+}
 
 export default function SmokesPage() {
   const circularSmokesColumns = [
@@ -32,90 +61,152 @@ export default function SmokesPage() {
     { name: "Regen", sortable: true },
   ];
 
+  const [isLoading, setIsLoading] = useState(true);
+  const [circularSmokesData, setCircularSmokesData] = useState<any[]>([]);
+  const [wallSmokesData, setWallSmokesData] = useState<any>([]);
+
+  const fetchData = async () => {
+    try {
+      const supabase = getSupabase();
+
+      const { data: circularSmokesData, error: circularSmokesError } =
+        await supabase
+          .from("abilities")
+          .select(
+            "id, name, icon_url, duration, radius, cost, regen, agents(*)",
+          )
+          .eq("category", "circular_smoke")
+          .order("name", { ascending: true });
+
+      if (circularSmokesError) {
+        console.error(circularSmokesError);
+      } else {
+        console.log(circularSmokesData);
+        setCircularSmokesData(circularSmokesData);
+      }
+
+      const { data: wallSmokesData, error: wallSmokesError } = await supabase
+        .from("abilities")
+        .select("id, name, icon_url, duration, length, cost, regen, agents(*)")
+        .eq("category", "wall_smoke")
+        .order("name", { ascending: true });
+
+      if (wallSmokesError) {
+        console.error(wallSmokesError);
+      } else {
+        console.log(wallSmokesData);
+        setWallSmokesData(wallSmokesData);
+      }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const CircularSmokesTable = () => {
+    return (
+      <Table aria-label="Circular Smokes" className="mt-4" fullWidth={true}>
+        <TableHeader>
+          {circularSmokesColumns.map((column) => (
+            <TableColumn key={column.name} allowsSorting={column.sortable}>
+              {column.name}
+            </TableColumn>
+          ))}
+        </TableHeader>
+
+        <TableBody isLoading={isLoading} items={circularSmokesData}>
+          {(smoke: CircularSmokeType) => (
+            <TableRow key={smoke.id}>
+              <TableCell>
+                <User
+                  avatarProps={{ src: smoke.agents.icon_url }}
+                  name={smoke.agents.name}
+                />
+              </TableCell>
+              <TableCell>
+                <Tooltip content={smoke.name}>
+                  <div className="cursor-pointer">
+                    <Image
+                      unoptimized
+                      alt={smoke.name}
+                      height={24}
+                      src={smoke.icon_url}
+                      width={24}
+                    />
+                  </div>
+                </Tooltip>
+              </TableCell>
+              <TableCell>{smoke.duration}</TableCell>
+              <TableCell>{smoke.radius}</TableCell>
+              <TableCell>{smoke.cost}</TableCell>
+              <TableCell>{smoke.regen ? smoke.regen : "x"}</TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    );
+  };
+
+  const WallSmokesTable = () => {
+    return (
+      <Table aria-label="Wall Smokes" className="mt-4">
+        <TableHeader>
+          {wallSmokesColumns.map((column) => (
+            <TableColumn key={column.name} allowsSorting={column.sortable}>
+              {column.name}
+            </TableColumn>
+          ))}
+        </TableHeader>
+
+        <TableBody isLoading={isLoading} items={wallSmokesData}>
+          {(smoke: WallSmokeType) => (
+            <TableRow key={smoke.id}>
+              <TableCell>
+                <User
+                  avatarProps={{ src: smoke.agents.icon_url }}
+                  name={smoke.agents.name}
+                />
+              </TableCell>
+              <TableCell>
+                <Tooltip content={smoke.name}>
+                  <div className="cursor-pointer">
+                    <Image
+                      unoptimized
+                      alt={smoke.name}
+                      height={24}
+                      src={smoke.icon_url}
+                      width={24}
+                    />
+                  </div>
+                </Tooltip>
+              </TableCell>
+              <TableCell>{smoke.duration}</TableCell>
+              <TableCell>{smoke.length}</TableCell>
+              <TableCell>{smoke.cost}</TableCell>
+              <TableCell>{smoke.regen ? smoke.regen : "x"}</TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <section>
       <h1 className={title()}>Smokes</h1>
       <div>
         <h2 className="mt-6">Circular Smokes</h2>
-        <Table className="mt-4" fullWidth={true}>
-          <TableHeader>
-            {circularSmokesColumns.map((column) => (
-              <TableColumn key={column.name}>{column.name}</TableColumn>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {smokesData.circularSmokesData.map((smoke) => (
-              <TableRow key={smoke.id}>
-                <TableCell>
-                  <User
-                    avatarProps={{ src: smoke.agent.agent_icon_url }}
-                    name={smoke.agent.name}
-                  />
-                </TableCell>
-                <TableCell>
-                  <Tooltip content={smoke.ability.name}>
-                    <div className="cursor-pointer">
-                      <Image
-                        unoptimized
-                        alt={smoke.ability.name}
-                        height={24}
-                        src={smoke.ability.iconUrl}
-                        width={24}
-                      />
-                    </div>
-                  </Tooltip>
-                </TableCell>
-                <TableCell>{smoke.duration}</TableCell>
-                <TableCell>{smoke.radius}</TableCell>
-                <TableCell>{smoke.cost}</TableCell>
-                <TableCell>
-                  {smoke.regen.reusable ? smoke.regen.regenTime : "x"}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        <CircularSmokesTable />
       </div>
+
       <div className="mt-6">
         <h2 className="mt-6">Wall Smokes</h2>
-        <Table className="mt-4">
-          <TableHeader>
-            {wallSmokesColumns.map((column) => (
-              <TableColumn key={column.name}>{column.name}</TableColumn>
-            ))}
-          </TableHeader>
-          <TableBody>
-            {smokesData.wallSmokesData.map((smoke) => (
-              <TableRow key={smoke.id}>
-                <TableCell>
-                  <User
-                    avatarProps={{ src: smoke.agent.agent_icon_url }}
-                    name={smoke.agent.name}
-                  />
-                </TableCell>
-                <TableCell>
-                  <Tooltip content={smoke.ability.name}>
-                    <div className="cursor-pointer">
-                      <Image
-                        unoptimized
-                        alt={smoke.ability.name}
-                        height={24}
-                        src={smoke.ability.iconUrl}
-                        width={24}
-                      />
-                    </div>
-                  </Tooltip>
-                </TableCell>
-                <TableCell>{smoke.duration}</TableCell>
-                <TableCell>{smoke.length}</TableCell>
-                <TableCell>{smoke.cost}</TableCell>
-                <TableCell>
-                  {smoke.regen.reusable ? smoke.regen.regenTime : "x"}
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
+        <WallSmokesTable />
       </div>
     </section>
   );
