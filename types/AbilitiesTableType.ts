27844ec6d7b5816ca62f593db11f28fa40c8e54d
LeightonGuang <leighton.guang@icloud.com>
2025-01-22 04:26:33 +0000

    Feat: New ability columns type

--- types/AbilitiesTableType.ts
+++ types/AbilitiesTableType.ts
@@ -1,13 +1,20 @@
 export interface AbilitiesTableType {
   id: number;
-  agent_id: number;
   name: string;
+  agent_id: number;
+  category: string;
   icon_url: string;
   key_bind: string;
   cost: number;
   charges_on_spawn: number;
   max_charge: number;
-  ult_orb_num: number;
+  ult_points: number;
+  health: number;
   cooldown: number;
+  regen: string;
+  duration: string;
+  length: number;
+  radius: number;
   description: string;
+  created_at: string;
 }
