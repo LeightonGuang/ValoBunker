bbaaca65ee008c99f44da8fb835c0b307101b1e4
LeightonGuang <leighton.guang@icloud.com>
2025-01-22 06:33:52 +0000

    Feat: New updated agent page layout

--- app/agents/[id]/page.tsx
+++ app/agents/[id]/page.tsx
@@ -2,7 +2,17 @@
 
 import { useParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import { Avatar, Card, CardBody, CardHeader, User } from "@nextui-org/react";
+import {
+  Card,
+  Image,
+  Avatar,
+  Divider,
+  Tooltip,
+  CardBody,
+  CardHeader,
+  Breadcrumbs,
+  BreadcrumbItem,
+} from "@nextui-org/react";
 
 import { getSupabase } from "@/utils/supabase/client";
 import { AgentsTableType } from "@/types/AgentsTableType";
@@ -12,74 +22,130 @@ const AgentPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [agentData, setAgentData] = useState<AgentsTableType>();
 
+  const abilitiesOrder = ["C", "Q", "E", "X"];
+
   const fetchData = async () => {
     try {
       const supabase = getSupabase();
-      const { data, error } = await supabase
+      const { data: agentData, error: agentError } = await supabase
         .from("agents")
         .select(`*, abilities(*), roles(*)`)
-        .eq("id", agentId);
+        .eq("id", agentId)
+        .single();
 
-      if (error) {
-        console.error(error);
+      if (agentError) {
+        console.error(agentError);
       } else {
-        setAgentData(data[0]);
+        setAgentData(agentData);
       }
     } catch (error) {
+      console.error(error);
     } finally {
       setIsLoading(false);
     }
   };
 
   const AgentCard = ({ className }: { className?: string }) => {
-    const abilitiesOrder = ["C", "Q", "E", "X"];
+    const ability = (bind: string) =>
+      agentData?.abilities.find((ability) => ability.key_bind === bind);
 
     return (
       <Card className={className}>
         <CardHeader className="flex justify-between">
           <div className="flex items-center gap-4">
-            <Avatar alt={agentData?.name} size="lg" src={agentData?.icon_url} />
+            <Avatar
+              alt={agentData?.name}
+              className="bg-transparent h-32 min-h-32 w-32 min-w-32 rounded-none"
+              src={agentData?.icon_url}
+            />
 
-            <span className="text-large">{agentData?.name}</span>
+            <div className="flex flex-col gap-1">
+              <span className="text-large">{agentData?.name}</span>
+
+              <span className="flex items-center gap-2 text-tiny">
+                <Avatar
+                  alt={agentData?.roles.name}
+                  className="bg-transparent h-4 w-4 rounded-none"
+                  src={agentData?.roles.icon_url}
+                />
+                <span>{agentData?.roles.name}</span>
+              </span>
+
+              <span className="text-tiny text-default-400">
+                Release Date:{" "}
+                {new Intl.DateTimeFormat().format(
+                  new Date(agentData?.release_date ?? ""),
+                )}
+              </span>
+            </div>
           </div>
-
-          <User
-            avatarProps={{
-              src: agentData?.roles.icon_url,
-              className: "h-6 w-6",
-            }}
-            name={agentData?.roles.name}
-          />
         </CardHeader>
 
+        <Divider />
+
         <CardBody>
-          <div>
-            <ul className="flex flex-col gap-2">
-              {abilitiesOrder.map((keybind) => {
-                const ability = agentData?.abilities.filter(
-                  (ability) => ability.key_bind === keybind,
-                )[0];
+          <div className="flex items-center justify-center gap-2">
+            {abilitiesOrder.map((bind, i) => {
+              return (
+                <div key={bind} className="flex items-center gap-2">
+                  <Tooltip key={bind} content={ability(bind)?.name}>
+                    <div className="flex w-full flex-col items-center text-center">
+                      <Image
+                        alt={ability(bind)?.name}
+                        className="flex h-8 w-8 flex-col rounded-none"
+                        src={ability(bind)?.icon_url}
+                      />
+                      <div className="my-2 flex h-3 max-w-7 flex-wrap items-center justify-center gap-1">
+                        {bind !== "X"
+                          ? Array.from({
+                              length: ability(bind)?.max_charge ?? 0,
+                            }).map((_, i) => (
+                              <div
+                                key={i}
+                                className="h-1 w-1 rounded-full bg-foreground"
+                              />
+                            ))
+                          : Array.from({
+                              length: ability(bind)?.ult_points ?? 0,
+                            }).map((_, i) => (
+                              <div key={i} className="relative h-1 w-1">
+                                <div className="absolute inset-0 rotate-45 transform bg-foreground" />
+                              </div>
+                            ))}
+                      </div>
+                      <span className="w-min">{bind}</span>
+                    </div>
+                  </Tooltip>
 
-                return (
-                  <div
-                    key={ability?.id}
-                    className="flex flex-col justify-start gap-2"
-                  >
-                    <User
-                      avatarProps={{
-                        src: ability?.icon_url,
-                        size: "md",
-                        className: "bg-transparent",
-                      }}
-                      className="w-max"
-                      name={ability?.name}
-                    />
+                  {i < abilitiesOrder.length - 1 && <Divider className="w-2" />}
+                </div>
+              );
+            })}
+          </div>
 
-                    <div>cost: {ability?.cost ? ability?.cost : "-"}</div>
+          <Divider className="my-3" />
+
+          <div className="flex flex-col gap-4">
+            {abilitiesOrder.map((bind) => {
+              return (
+                <div key={bind}>
+                  <div className="flex items-center gap-2">
+                    <span className="text-medium">{ability(bind)?.name}</span>
+                    <span className="text-tiny text-default-400">
+                      {ability(bind)?.cost
+                        ? ability(bind)?.cost
+                        : ability(bind)?.cost === 0
+                          ? "Free"
+                          : ability(bind)?.cost === null
+                            ? `${ability(bind)?.ult_points ?? "x"} ult points`
+                            : ""}
+                    </span>
                   </div>
-                );
-              })}
-            </ul>
+
+                  <p className="mt-2 text-tiny">{ability(bind)?.description}</p>
+                </div>
+              );
+            })}
           </div>
         </CardBody>
       </Card>
@@ -92,7 +158,18 @@ const AgentPage = () => {
 
   return (
     <section className="flex justify-center">
-      {!isLoading && <AgentCard className="w-96" />}
+      {!isLoading && (
+        <div className="w-full">
+          <Breadcrumbs>
+            <BreadcrumbItem href="/agents">Agents</BreadcrumbItem>
+            <BreadcrumbItem>{agentData?.name}</BreadcrumbItem>
+          </Breadcrumbs>
+
+          <div className="flex justify-center">
+            <AgentCard className="mt-4 w-96" />
+          </div>
+        </div>
+      )}
     </section>
   );
 };
