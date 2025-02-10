648aa80d94b6033cf46d8f28b5533a6af8d296c2
LeightonGuang <leighton.guang@icloud.com>
2025-02-10 16:51:07 +0000

    Fix: Rename minimap_url to minimap_img_url

--- types/MapsTableType.ts
+++ types/MapsTableType.ts
@@ -2,7 +2,7 @@ export interface MapsTableType {
   id: number;
   name: string;
   cover_img_url: string;
-  mini_map_url: string;
+  minimap_img_url: string;
   location: string;
   competitive_map_pool: boolean;
   site_count: number;
