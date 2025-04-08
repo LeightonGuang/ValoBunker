7a6ed11ad06d4198ee64a5c8a8ae426ec7b57471
LeightonGuang <leighton.guang@icloud.com>
2025-04-08 15:20:45 +0100

    Feat: Add optional fields to all interface keys

--- types/CountdownTableType.ts
+++ types/CountdownTableType.ts
@@ -1,9 +1,9 @@
 export interface CountdownTableType {
-  id: number;
-  name: string;
-  img_url: string;
-  is_same_time: boolean;
-  start_date: Date;
-  end_date: Date;
-  created_at: string;
+  id?: number;
+  name?: string;
+  img_url?: string;
+  is_same_time?: boolean;
+  start_date?: Date;
+  end_date?: Date;
+  created_at?: string;
 }
