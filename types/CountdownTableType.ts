d80851000a05a9e3518da21578680f17a4341b01
LeightonGuang <leighton.guang@icloud.com>
2025-05-19 05:58:08 +0100

    Refactor: Update CountdownTableType to allow null values for start_date and end_date

--- types/CountdownTableType.ts
+++ types/CountdownTableType.ts
@@ -3,7 +3,7 @@ export interface CountdownTableType {
   name?: string;
   img_url?: string;
   is_same_time?: boolean;
-  start_date?: Date;
-  end_date?: Date;
+  start_date?: Date | null;
+  end_date?: Date | null;
   created_at?: string;
 }
